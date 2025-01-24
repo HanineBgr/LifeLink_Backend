3a2d8e50592c7e519f188c48ffbb87f1a2c8740e
hanine bouguerra <bouguerra.hanine@esprit.tn>
2025-01-24 16:53:24 +0100

    Data Cleaning

--- train_model.py
+++ train_model.py
@@ -5,8 +5,19 @@ doctor_versus_disease_data = pd.read_csv('health_tips/Doctor_Versus_Disease.csv'
 disease_description_data = pd.read_csv('health_tips/Disease_Description.csv', encoding='ISO-8859-1')
 
 
+print("Missing values in symptom_weights_data:")
+print(symptom_weights_data.isnull().sum())
 
-print(symptom_weights_data.head())  
-print(doctor_versus_disease_data.head())
-print(disease_description_data.head())  
+print("Missing values in doctor_versus_disease_data:")
+print(doctor_versus_disease_data.isnull().sum())
 
+print("Missing values in disease_description_data:")
+print(disease_description_data.isnull().sum())
+print(symptom_weights_data.describe())
+print(doctor_versus_disease_data.describe())
+print(disease_description_data.describe())
+
+# Check for unique values in categorical columns
+print(symptom_weights_data['abdominal_pain'].unique())
+print(doctor_versus_disease_data['Drug Reaction'].unique())
+print(disease_description_data['Disease'].unique())
