bdbec904af54597c9d40a79773e862ee8db722f1
hanine bouguerra <bouguerra.hanine@esprit.tn>
2025-01-24 13:59:04 +0100

    updating the csv files

--- train_model.py
+++ train_model.py
@@ -1,12 +1,8 @@
 import pandas as pd
 
-# Load the CSV files
-symptom_weights = pd.read_csv('Symptom_Weights.csv')
-specialist_data = pd.read_csv('Specialist.csv')
-original_data = pd.read_csv('Original_Dataset.csv')
-doctor_versus_disease = pd.read_csv('Doctor_Versus_Disease.csv')
-doctor_specialist = pd.read_csv('Doctor_Specialist.csv')
-disease_description = pd.read_csv('Disease_Description.csv')
+# Load the CSV files from the project folder
+specialist_data = pd.read_csv(r'C:\Users\Dell\Desktop\LifeLink_Backend\Specialist.csv')
+
 
 # Check the first few rows of the data to understand the structure
 print(symptom_weights.head())
